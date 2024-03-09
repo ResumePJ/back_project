9f108556af39fb1173ffa77d1fcfb217617d1bf7
OhSoomin812 <o3o_121@naver.com>
2024-03-09 16:27:28 +0900

    FEAT : 개발 환경 설정

---
+++ src/main/java/com/resume/resu/ResuApplication.java
@@ -0,0 +1,13 @@
+package com.resume.resu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResuApplication.class, args);
+	}
+
+}
