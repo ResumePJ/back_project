fb7515d67148ee3f100c14dd59da47b77c2ceaca
천아현 <fofi7889@naver.com>
2024-04-08 19:46:07 +0900

    :bug: Fix: "기본정보 불러오기 기능 수정"
    회원 테이블 정보가 아닌, 이력서의 기본 정보 불러오도록 수정

---
+++ src/main/java/com/resume/resu/vo/response/ResumeBasicInfoDTO.java
@@ -0,0 +1,31 @@
+package com.resume.resu.vo.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class ResumeBasicInfoDTO {
+    public int resumeNo;
+    public int memberNo;
+
+    //tinyint(1) 타입
+    public int type;
+
+    public String name;
+
+    /* TODO :  타입 수정 해야 할 지도? - tinyint(1) 타입*/
+    public boolean gender;
+
+    public LocalDate birth;
+
+    public String phone;
+    public String address;
+    public String photo;
+    public String intro;
+    public String resumeTitle;
+    public boolean basic;
+
+}
