f5357a6052ea2bc211c3291003a81726ec59b7e0
천아현 <fofi7889@naver.com>
2024-04-22 05:20:58 +0900

    :bug: Fix: "member 테이블의 전화번호 컬럼 데이터 수정"
    member 테이블의 전화번호 컬럼 값에서 '-' 삭제
    
    Resolves: #4

--- src/main/resources/schema/data.sql
+++ src/main/resources/schema/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO resudb.member(email,password,name,phone,birth,joinDate,type) values ('test@test.com','test','testname','010-1234-5678','1999-01-01','2024-03-13',0);
-INSERT INTO resudb.member(email,password,name,phone,birth,joinDate,type) values ('test2@test.com','test2','testname2','010-2234-5678','1999-02-01','2024-02-13',0);
+INSERT INTO resudb.member(email,password,name,phone,birth,joinDate,type) values ('test@test.com','test','testname','01012345678','1999-01-01','2024-03-13',0);
+INSERT INTO resudb.member(email,password,name,phone,birth,joinDate,type) values ('test2@test.com','test2','testname2','01022345678','1999-02-01','2024-02-13',0);
 
 
 
 
-INSERT INTO resudb.resume values (null,1,0,'내이름',1,'1999-01-01','010-1234-5678','주소다','자기소개','이력서이름',0);
-INSERT INTO resudb.resume values (null,2,0,'내이름2',1,'1999-02-01','010-2234-5678','주소다','자기소개','이력서이름',0);
+INSERT INTO resudb.resume values (null,1,0,'내이름',1,'1999-01-01','01012345678','주소다','자기소개','이력서이름',0);
+INSERT INTO resudb.resume values (null,2,0,'내이름2',1,'1999-02-01','01022345678','주소다','자기소개','이력서이름',0);
