742eeb730bf9af9943632b1b8901d3e2248f9315
천아현 <fofi7889@naver.com>
2024-05-07 01:06:33 +0900

    :bug: Fix: "@Controller -> @RestController로 변경"
    @Controller -> @RestController로 변경

--- src/main/java/com/resume/resu/controller/email/EmailController.java
+++ src/main/java/com/resume/resu/controller/email/EmailController.java
@@ -12,10 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
-@Controller
+@RestController
 @Slf4j
 @RequiredArgsConstructor
 public class EmailController {
